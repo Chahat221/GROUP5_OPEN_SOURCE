6b814a9203b080b6a5ee762b5e17f21313588f8d
Sanianoorkhan123 <ssania0079@conestogac.on.ca>
2026-03-25 15:30:29 -0400

    KAN-4 Implement CreateBooking use case

--- src/application/use-cases/CreateBooking.ts
+++ src/application/use-cases/CreateBooking.ts
@@ -1,5 +1,6 @@
 import { Booking } from "../../domain/entities/Booking";
 
+// Use case for creating a booking
 export class CreateBooking {
   execute(userId: string, date: string): Booking {
     const newBooking: Booking = {
