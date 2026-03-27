6e14dd696db83c598b470230b456f739bb76938f
Chahat221 <chahatgautam6@gmail.com>
2026-03-27 19:45:28 +0000

    KAN-4 Fix LoginUser import path

--- src/application/use-cases/LoginUser.ts
+++ src/application/use-cases/LoginUser.ts
@@ -1,19 +1,15 @@
-import { User } from "../domain/entities/user";
+import { User } from "../../domain/entities/user";
 
 export class LoginUser {
-
   execute(email: string, password: string): User | null {
-
     if (email === "test@test.com" && password === "123456") {
-
       return {
         id: "1",
         name: "Test User",
-        email: email,
-        password: password,
+        email,
+        password,
         role: "user"
       };
-
     }
 
     return null;
--- src/application/use-cases/RegisterUser.ts
+++ src/application/use-cases/RegisterUser.ts
@@ -1,24 +1,12 @@
-<<<<<<< HEAD
-import { User } from "../domain/entities/user";
-
-=======
 import { User } from "../../domain/entities/user";
 
->>>>>>> 3dd44ace3f2ba210f267f9b2a348466d5ecebf70
 export class RegisterUser {
   execute(name: string, email: string, password: string): User {
     const newUser: User = {
-<<<<<<< HEAD
-      id: Date.now().toString(),
-      name: name,
-      email: email,
-      password: password,
-=======
       id: Date.now().toString(),
       name,
       email,
       password,
->>>>>>> 3dd44ace3f2ba210f267f9b2a348466d5ecebf70
       role: "user"
     };
 
