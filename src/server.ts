8bebc1f20fd23f3eae04df22993dab2693fd9540
Chahat221 <chahatgautam6@gmail.com>
2026-03-27 19:39:43 +0000

    KAN-4 Add booking routes

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import { AuthController } from "./controllers/AuthController";
+import { BookingController } from "./controllers/BookingController";
 
 const app = express();
 
 app.use(express.json());
 
+// Authentication routes
 app.post("/register", AuthController.register);
 app.post("/login", AuthController.login);
+
+// Booking routes
+app.post("/bookings", BookingController.create);
+app.get("/bookings", BookingController.getAll);
 
 const PORT = 3000;
 
