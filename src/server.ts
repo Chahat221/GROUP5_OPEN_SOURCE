1b1b406e030d241e4950c53aefa1d723e483ac21
Sanianoorkhan123 <ssania0079@conestogac.on.ca>
2026-04-10 12:25:34 -0400

    Fix merge conflicts in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,8 @@
 import express from "express";
 import { AuthController } from "./controllers/AuthController";
 import { BookingController } from "./controllers/BookingController";
-<<<<<<< HEAD
-import { EventController } from "./controllers/EventController";
-=======
 import { EventController } from "./controllers/EventController";
 
->>>>>>> 0bd8cf7e5b074ba98d5501d63a648ae6b6aefa6b
 const app = express();
 
 app.use(express.json());
@@ -19,12 +15,7 @@ app.post("/login", AuthController.login);
 app.post("/bookings", BookingController.create);
 app.get("/bookings", BookingController.getAll);
 app.delete("/bookings/:id", BookingController.cancel);
-<<<<<<< HEAD
-app.post("/events", EventController.create);
-app.get("/events", EventController.getAll);
-=======
 app.patch("/bookings/:id/status", BookingController.updateStatus);
->>>>>>> 0bd8cf7e5b074ba98d5501d63a648ae6b6aefa6b
 
 // Event routes
 app.post("/events", EventController.create);
